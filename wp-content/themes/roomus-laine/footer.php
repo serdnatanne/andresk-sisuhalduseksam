3dd3de5741b142191a3fbb0d11208cdd147bf416
Andres Kuusk <serdnatanne@gmail.com>
2026-06-11 11:58:55 +0300

    add footer eksamitoo nr

--- wp-content/themes/roomus-laine/footer.php
+++ wp-content/themes/roomus-laine/footer.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
     <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> Rõõmus Laine OÜ. Kõik õigused kaitstud.</p>
+        <p>&copy; <?php echo date('Y'); ?> Rõõmus Laine OÜ. Kõik õigused kaitstud. Eksamitöö: EX-2026-G362</p>
     </div>
 </footer>
 
