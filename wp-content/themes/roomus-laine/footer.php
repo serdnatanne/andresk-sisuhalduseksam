4b2c1b64d163abb6d01c88da9da74836cfa32986
Andres Kuusk <serdnatanne@gmail.com>
2026-06-21 15:57:44 +0300

    add how-went to readme333

--- wp-content/themes/roomus-laine/footer.php
+++ wp-content/themes/roomus-laine/footer.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
     <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> Rõõmus Laine OÜ. Kõik õigused kaitstud. Eksamitöö: EX-2026-G362</p>
+        <p>&copy; <?php echo date('Y'); ?>Tere Madis... Rõõmus Laine OÜ. Kõik õigused kaitstud. Eksamitöö: EX-2026-G362</p>
     </div>
 </footer>
 
